f629cf58074a602b41abe431aaa91a45367eee1a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-10-25 11:02:46 -0400

    DTL Coupon Page Demo 1.3  /site.js

---
+++ site.js
@@ -0,0 +1,8 @@
+var site_json = {
+                    "name" : "The District at Tustin Legacy",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f2b1016e6f645d03550000/image/jpeg/undefined/dtl_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "//thedistrict.mallmaverick.com/api/v2/district/social.json"
+                }
+                
+$.cookie.json = true;
